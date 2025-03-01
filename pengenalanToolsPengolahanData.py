c989d7a7bea119c06c44be1faa628ef41b29fd16
echan <akkitherythm@gmail.com>
2025-03-02 05:53:32 +0700

    Mencoba pandas dan SciPy

--- pengenalanToolsPengolahanData.py
+++ pengenalanToolsPengolahanData.py
@@ -1,4 +1,19 @@
 import numpy as np
+import pandas as pd
+import scipy
 
+# Example for using numpy
 array_1 = np.array([2, 3, 6, 5])
 print(array_1)
+
+# Example for using pandas
+data = {
+    'Name' : ['Lulu', 'Lala', 'Lili'],
+    'Age': [23, 45, 46],
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+#Example for using SciPy
+print(f'SciPy version is {scipy.__version__}')
